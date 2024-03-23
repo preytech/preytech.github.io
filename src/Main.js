3d3eb4e14d5df4df52230a08720abcc517450862
preytech <pimonov.makar@mail.ru>
2024-03-23 13:09:25 +0300

    я всё сломал

--- src/Main.js
+++ src/Main.js
@@ -23,23 +23,25 @@ function Main()
                         <p>Ваша улыбка -</p>
                         <p className = "text-Blue">наш приоритет</p>
                     </div>
-                    <div className = "flex flex-col items-center gap-[20px] font-Light relative w-[200px]">
-                        <button className = "bg-Blue text-white px-12 rounded-lg font-Light">
-                            <p>Акции и</p>
-                            <p>предложения</p> 
-                        </button>
-                        <button className = "border-Blue border rounded-lg px-6 py-1 flex items-center gap-2.5">
-                            <p className = "text-black font-Light">Подробнее</p>
-                            <img src = {Arrow} alt = ""/>
-                        </button>
-                        <img className = "absolute bottom-[110px] right-[200px]" src = {BlackMicroStar1} alt = ""/>
-                        <img className = "absolute top-20 right-[185px]" src = {BlueMicroStar1} alt = ""/>
-                        <img className = "absolute left-[190px] top-[70px]" src = {BlueArrow} alt = ""/>
+                    <div>
+                        <div className = "flex flex-col gap-[20px] font-Light relative justify-center">
+                            <button className = "bg-Blue text-white px-12 rounded-lg font-Light">
+                                <p>Акции и</p>
+                                <p>предложения</p> 
+                            </button>
+                            <button className = "border-Blue border rounded-lg px-6 py-1 flex items-center gap-2.5">
+                                <p className = "text-black font-Light">Подробнее</p>
+                                <img src = {Arrow} alt = ""/>
+                            </button>
+                                <img className = "absolute bottom-[110px] right-[200px]" src = {BlackMicroStar1} alt = ""/>
+                                <img className = "absolute top-20 right-[185px]" src = {BlueMicroStar1} alt = ""/>
+                                <img className = "absolute left-[190px] top-[70px]" src = {BlueArrow} alt = ""/>
+                            <div className = "relative w-[160px] flex self-center text-sm">
+                                <p className = "font-Light">Начните знакомство с нашей стоматологией выгодно</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className = "relative w-[160px] flex self-center text-sm">
-                        <p className = "font-Light">Начните знакомство с нашей стоматологией выгодно</p>
-                    </div>
-                </div>
+                </div>            
                 <div className = "relative">
                     <img className = "rounded-full w-[580px] h-[580px] object-cover" src = {Kid} alt = ""/>
                     <img className = "absolute top-20 left-[450px] w-[184px] h-[150px]" src = {Circle} alt = ""/>
